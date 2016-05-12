a6c51c5de2db7e27d1b684546db87aecdca26393
Magne Land <magne.land@appfolio.com>
2016-05-12 15:30:33 -0700

    ml - first real commit
    See README for details

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # bundler-diff
+
+Examines a GitHub pull request for Gemfile.lock changes.
+
+# Usage
+
+Open a GitHub pull request in your favorite browser:
+
+http://github.com/<org>/<repo>/pull/<pr>.diff
+
+Save diff file locally.
+
+Run bundler-diff on your diff file:
+
+    ruby ./bundler-diff.rb --file <diff-file>
+
+Inject your favorite gem server:
+
+    GEM_COMPARE_SOURCES='<GEM_SERVER_URL>' ruby ./bundler-diff.rb --file <diff-file>
+
+Pass on parameters to gem compare:
+
+    ruby ./bundler-diff.rb --file <diff-file> -- --files
---
+++ bundler-diff.rb
@@ -0,0 +1,79 @@
+require 'rubygems'
+require 'bundler/setup'
+
+require 'optparse'
+
+GEM_NAME = /[a-zA-Z0-9_-]+/
+SPECIFIC_GEM_VERSION = /[0-9]+\.[0-9]+\.[0-9]+(\.[a-zA-Z0-9_-]+)?/
+
+def parse_file(options = {}, git_compare_options = {})
+
+  # Now we can use the options hash however we like.
+  puts "Examining #{ options[:file] }" if options[:file]
+
+  removed_gems = {}
+  added_gems = {}
+  file_under_examination = nil
+  IO.foreach(options[:file]) do |line|
+    case line
+      when /^(diff .*)$/
+        if ($1 =~ /^diff --git a\/(.*)Gemfile.lock b\/((\1)Gemfile.lock)/)
+          file_under_examination = $2
+          puts line
+        elsif file_under_examination
+          compare_gems(removed_gems, added_gems, git_compare_options)
+          file_under_examination = nil
+          removed_gems = {}
+          added_gems = {}
+        end
+      when /^\-\s+(#{GEM_NAME}) \((#{SPECIFIC_GEM_VERSION})\)/
+        if file_under_examination
+          # print "gem #{$1} version #{$2} removed: ", line
+          removed_gems[$1] = $2
+        end
+      when /^\+\s+(#{GEM_NAME}) \((#{SPECIFIC_GEM_VERSION})\)/
+        if file_under_examination
+          # print "gem #{$1} version #{$2} added: ", line
+          added_gems[$1] = $2
+        end
+    end
+  end
+end
+
+def compare_gems(removed_gems={}, added_gems={}, git_compare_options={})
+  puts "removed_gems: #{removed_gems.keys - added_gems.keys}"
+  puts "added_gems: #{added_gems.keys - removed_gems.keys}"
+  changed_gems = {}
+  removed_gems.each do |gem_name, old_gem_version|
+    changed_gems[gem_name] = {'old' => old_gem_version, 'new' => added_gems[gem_name]} if added_gems.has_key?(gem_name)
+  end
+  puts "changed_gems: #{changed_gems.keys}"
+
+  changed_gems.each do |gem_name, h|
+    compare_gem(gem_name, h['old'], h['new'], git_compare_options)
+  end
+end
+
+def compare_gem(gem_name, old_gem_version, new_gem_version, git_compare_options)
+  sources = ENV['GEM_COMPARE_SOURCES'] ? "--sources #{ENV['GEM_COMPARE_SOURCES']}" : ''
+  cmd = "gem compare #{sources} #{gem_name} #{old_gem_version} #{new_gem_version} #{git_compare_options}"
+  puts cmd
+  output = `#{cmd}`
+  puts output
+end
+
+options = {}
+options_parser = OptionParser.new do |parser|
+  parser.on("-f", "--file FILENAME", "The name of the Pull Request diff file to examine.") do |v|
+    options[:file] = v
+  end
+  parser.on("--", "The rest of options will be passed on to git compare.") do |v|
+    # no-op here
+  end
+end
+
+both_args = $*.join(" ").split(" -- ")
+options_parser.parse!(both_args[0].split(' '))
+git_compare_options = both_args[1]
+
+parse_file(options, git_compare_options)
