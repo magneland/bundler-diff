cd594af97bafff051af175171b81913258d32640
Magne Land <magne.land@appfolio.com>
2016-05-12 18:04:11 -0700

    ml - cache the gem changes
    Apply DRY principle for multiple Gemfile.lock files with identical changes

--- bundler-diff.rb
+++ bundler-diff.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'bundler/setup'
 
 require 'optparse'
+require 'set'
 
 GEM_NAME = /[a-zA-Z0-9_-]+/
 SPECIFIC_GEM_VERSION = /[0-9]+\.[0-9]+\.[0-9]+(\.[a-zA-Z0-9_-]+)?/
@@ -11,6 +12,7 @@ def parse_file(options = {}, git_compare_options = {})
   # Now we can use the options hash however we like.
   puts "Examining #{ options[:file] }" if options[:file]
 
+  @all_changed_gems = Set.new
   removed_gems = {}
   added_gems = {}
   file_under_examination = nil
@@ -55,12 +57,22 @@ def compare_gems(removed_gems={}, added_gems={}, git_compare_options={})
 end
 
 def compare_gem(gem_name, old_gem_version, new_gem_version, git_compare_options)
-  sources = ENV['GEM_COMPARE_SOURCES'] ? "--sources #{ENV['GEM_COMPARE_SOURCES']}" : ''
-  cmd = "gem compare #{sources} #{gem_name} #{old_gem_version} #{new_gem_version} #{git_compare_options}"
+  cmd = "gem compare #{gem_name} #{old_gem_version} #{new_gem_version} #{git_compare_options}"
   puts cmd
+
+  if @all_changed_gems.include?(cmd)
+    puts "(already compared this earlier)"
+    return
+  end
+  @all_changed_gems.add(cmd)
   output = `#{cmd}`
   puts output
 end
+
+cmd = "gem sources --add #{ENV['GEM_COMPARE_SOURCES']}"
+puts cmd
+output = `#{cmd}`
+puts output
 
 options = {}
 options_parser = OptionParser.new do |parser|
@@ -77,3 +89,8 @@ options_parser.parse!(both_args[0].split(' '))
 git_compare_options = both_args[1]
 
 parse_file(options, git_compare_options)
+
+cmd = "gem sources --remove #{ENV['GEM_COMPARE_SOURCES']}"
+puts cmd
+output = `#{cmd}`
+puts output
